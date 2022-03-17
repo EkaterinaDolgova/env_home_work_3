d25cdff654dbe671aa997d85e34460fa06131a8c
katya <kotenokches@rambler.ru>
2022-03-17 10:24:47 +0500

    Домашка исправлена, но не допонимаю - как в интерфейс вынести методы getModelName и getWheelsCount

--- src/skyro/company/Bycicle.java
+++ src/skyro/company/Bycicle.java
@@ -1,41 +1,24 @@
 package skyro.company;
-
-<<<<<<< HEAD
 public class Bycicle implements StationInterface {
-=======
-public class Bycicle implements CarInterface {
->>>>>>> origin/env-home_work3
     public String modelName;
     public int wheelsCount;
 
-    public Bycicle (String modelName,int wheelsCount){
-        this.modelName=modelName;
-        this.wheelsCount=wheelsCount;
+    public Bycicle(String modelName, int wheelsCount) {
+        this.modelName = modelName;
+        this.wheelsCount = wheelsCount;
     }
-    public String getModelName(){
+
+    public String getModelName() {
         return this.modelName;
     }
-<<<<<<< HEAD
-    public int getWheelsCount(){
+
+    public int getWheelsCount() {
         return this.wheelsCount;
     }
-=======
 
-    public int getWheelsCount(){
-        return this.wheelsCount;
-    }
-    public void setModelName(String modelName){
-        this.modelName=modelName;
-    }
-
-    public void setWheelsCount(int wheelsCount){
-        this.wheelsCount=wheelsCount;
-    }
-
->>>>>>> origin/env-home_work3
     @Override
     public void updateTyre() {
         System.out.println("Меняем покрышку");
     }
-
 }
+
